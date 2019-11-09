a3e6af8fa24596b24d7ae2ea2de20e8aad56e24b
kumatheworld <kumatheworld1105@yahoo.co.jp>
2019-11-09 16:22:46 +0900

    Add networks using nn.Sequential

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from nn_modules import KumaFC, KumaCNN
+from nn_modules import KumaFC, KumaFCSeq, KumaCNN, KumaCNNSeq
 import train
 
 if __name__ == '__main__':
-    train.main(KumaFC)
-
+    train.main(KumaCNN)
--- nn_modules.py
+++ nn_modules.py
@@ -1,20 +1,39 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+size_in = 28 * 28
+size_out = 10
+
+
 class KumaFC(nn.Module):
     def __init__(self):
         super().__init__()
-        self.size_in = 28 * 28
         self.size_hidden = 100
-        self.size_out = 10
-        self.fc1 = nn.Linear(self.size_in, self.size_hidden)
-        self.fc2 = nn.Linear(self.size_hidden, self.size_out)
+        self.fc1 = nn.Linear(size_in, self.size_hidden)
+        self.fc2 = nn.Linear(self.size_hidden, size_out)
 
     def forward(self, x):
-        x = x.view(-1, self.size_in)
+        x = x.view(-1, size_in)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+class KumaFCSeq(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.size_hidden = 100
+        self.layers = nn.Sequential(
+            nn.Linear(size_in, self.size_hidden),
+            nn.ReLU(inplace=True),
+            nn.Linear(self.size_hidden, size_out),
+            nn.LogSoftmax()
+        )
+
+    def forward(self, x):
+        x = x.view(-1, size_in)
+        x = self.layers(x)
+        return x
+
 
 class KumaCNN(nn.Module):
     def __init__(self):
@@ -33,3 +52,27 @@ class KumaCNN(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+
+class KumaCNNSeq(nn.Module):
+    def __init__(self):
+        super(KumaCNNSeq, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 20, 5, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(20, 50, 5, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2, 2)
+        )
+        self.layers = nn.Sequential(
+            nn.Linear(4*4*50, 500),
+            nn.ReLU(inplace=True),
+            nn.Linear(500, 10),
+            nn.LogSoftmax()
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(-1, 4*4*50)
+        x = self.layers(x)
+        return x
