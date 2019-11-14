10e8ff50e225489fe0f94cdcdedf6ace7a3b13c6
kumatheworld <kumatheworld1105@yahoo.co.jp>
2019-11-15 00:08:37 +0900

    Make the handmade network more abstract

--- handmade_fc.py
+++ handmade_fc.py
@@ -4,33 +4,81 @@ from scipy.special import logsumexp, softmax
 from torchvision import datasets, transforms
 import torch
 
-class HandmadeFC():
+class HandmadeTensor():
+    def __init__(self, val):
+        self.val = val
+        self.grad = 0
+        self.velocity = 0
+
+class HandmadeNN():
     def __init__(self):
         self.size_in = 28 * 28
-        self.size_hidden = 100
         self.size_out = 10
+        self.params = {}
 
+    def xavier_init_linear(self, size):
+        if isinstance(size, (tuple, list)):
+            size_in = size[0]
+        else:
+            size_in = size
         bd = 1 / math.sqrt(self.size_in)
-        self.w1 = np.random.uniform(-bd, bd, [self.size_in, self.size_hidden])
-        self.b1 = np.random.uniform(-bd, bd, self.size_hidden)
-        bd = 1 / math.sqrt(self.size_hidden)
-        self.w2 = np.random.uniform(-bd, bd, [self.size_hidden, self.size_out])
-        self.b2 = np.random.uniform(-bd, bd, self.size_out)
+        return np.random.uniform(-bd, bd, size)
+
+    def add_linear(self, name, size):
+        self.params[name] = HandmadeTensor(self.xavier_init_linear(size))
 
     def forward(self, x):
-        self.x = x.reshape(-1, self.size_in)
-        self.z1 = self.x.dot(self.w1) + self.b1
-        self.a1 = np.maximum(self.z1, 0)
-        self.z2 = self.a1.dot(self.w2) + self.b2
-        self.a2 = self.z2 - logsumexp(self.z2, axis=1).reshape(-1, 1)
+        pass
 
-    def loss(self, a2, y):
+    def loss(self, out, y):
         n = y.shape[0]
         l = 0
         for i in range(n):
-            l -= a2[i][y[i]]
+            l -= out[i][y[i]]
         l = l / n
         return l
+
+    def backward(self, y):
+        pass
+
+    def init_velocity(self):
+        for par in self.params.values():
+            par.velocity = 0
+
+    def update(self, lr, rho):
+        for par in self.params.values():
+            par.velocity = rho * par.velocity + par.grad
+            par.val -= lr * par.velocity
+
+    def train(self, x, y, lr, rho):
+        self.init_velocity()
+        out = self.forward(x)
+        self.loss_train = self.loss(out, y)
+        self.backward(y)
+        self.update(lr, rho)
+
+    def test(self, x, y):
+        out = self.forward(x)
+        self.loss_test = self.loss(out, y)
+
+
+class HandmadeFC(HandmadeNN):
+    def __init__(self):
+        super().__init__()
+        self.size_hidden = 100
+
+        self.add_linear('w1', (self.size_in, self.size_hidden))
+        self.add_linear('b1', self.size_hidden)
+        self.add_linear('w2', (self.size_hidden, self.size_out))
+        self.add_linear('b2', (self.size_out))
+
+    def forward(self, x):
+        self.x = x.reshape(-1, self.size_in)
+        self.z1 = self.x.dot(self.params['w1'].val) + self.params['b1'].val
+        self.a1 = np.maximum(self.z1, 0)
+        self.z2 = self.a1.dot(self.params['w2'].val) + self.params['b2'].val
+        self.a2 = self.z2 - logsumexp(self.z2, axis=1).reshape(-1, 1)
+        return self.a2
 
     def backward(self, y):
         n = y.shape[0]
@@ -40,43 +88,16 @@ class HandmadeFC():
             self.grad_a2[i][y[i]] -= 1 / n
         self.grad_z2 = softmax(self.z2, axis=1) / n + self.grad_a2
 
-        self.grad_b2 = self.grad_z2.sum(axis=0)
-        self.grad_w2 = np.transpose(self.a1).dot(self.grad_z2)
+        self.params['b2'].grad = self.grad_z2.sum(axis=0)
+        self.params['w2'].grad = np.transpose(self.a1).dot(self.grad_z2)
 
-        self.grad_a1 = self.grad_z2.dot(np.transpose(self.w2))
+        self.grad_a1 = self.grad_z2.dot(np.transpose(self.params['w2'].val))
         self.grad_z1 = (self.a1 > 0) * self.grad_a1
 
-        self.grad_b1 = self.grad_z1.sum(axis=0)
-        self.grad_w1 = np.transpose(self.x).dot(self.grad_z1)
+        self.params['b1'].grad = self.grad_z1.sum(axis=0)
+        self.params['w1'].grad = np.transpose(self.x).dot(self.grad_z1)
 
-        self.grad_x = self.grad_z1.dot(np.transpose(self.w1))
-
-    def init(self):
-        self.vw1 = 0
-        self.vb1 = 0
-        self.vw2 = 0
-        self.vb2 = 0
-
-    def update(self, lr, rho):
-        self.vb2 = rho * self.vb2 + self.grad_b2
-        self.b2 -= lr * self.vb2
-        self.vw2 = rho * self.vw2 + self.grad_w2
-        self.w2 -= lr * self.vw2
-        self.vb1 = rho * self.vb1 + self.grad_b1
-        self.b1 -= lr * self.vb1
-        self.vw1 = rho * self.vw1 + self.grad_w1
-        self.w1 -= lr * self.vw1
-
-    def train(self, x, y, lr, rho):
-        self.init()
-        self.forward(x)
-        self.loss_train = self.loss(self.a2, y)
-        self.backward(y)
-        self.update(lr, rho)
-
-    def test(self, x, y):
-        self.forward(x)
-        self.loss_test = self.loss(self.a2, y)
+        self.grad_x = self.grad_z1.dot(np.transpose(self.params['w1'].val))
 
 
 def main():
